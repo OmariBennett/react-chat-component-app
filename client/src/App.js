76d2d6a72ffe5962df76671e2cf6a53d17538308
OmariBennett <omari.bennett@gmail.com>
2021-08-20 14:58:21 -0400

    Adds the header for chat component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 export default function App() {
 	return (
-		<section>
-			<h1>Chat App</h1>
-			<form onSubmit={(e) => e.preventDefault()}>
-				<label for='name'>Name:</label>{' '}
-				<input id='name' type='text' value='' placeholder='Enter user name' />
-				<label for='chatRoom'>Join a Chat Room</label>
-				<input id='chatRoom' type='submit' value='Join' />
-			</form>
-		</section>
+		<div>
+			{/* Login form component */}
+			<section>
+				<h1>Chat App</h1>
+				<form onSubmit={(e) => e.preventDefault()}>
+					<label for='name'>Name:</label>{' '}
+					<input id='name' type='text' value='' placeholder='Enter user name' />
+					<label for='chatRoom'>Join a Chat Room</label>
+					<input id='chatRoom' type='submit' value='Join' />
+				</form>
+			</section>
+			{/* Login form component */}
+			<section>
+				<header>
+					<span>logo placeholder</span>
+					<button>close button placeholder</button>
+				</header>
+			</section>
+		</div>
 	);
 }
